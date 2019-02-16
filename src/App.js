b4da6240464c8ad2afe385d6ac1165d74f9cb9c4
divya <divu333@gmail.com>
2019-02-16 12:49:30 -0700

    part of part1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Nav from "./components/Nav/Nav";
-import Auth from "./components/Auth/Auth";
-import Form from "./components/Form/Form";
-import Post from "./components/Post/Post";
+import logo from "../src/helo_logo.png";
+
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import Nav from "./components/Nav/Nav";
+// import Auth from "./components/Auth/Auth";
+// import Form from "./components/Form/Form";
+// import Post from "./components/Post/Post";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
-        <Dashboard />
-        <Auth />
-        <Form />
-        <Post />
+        <div className="auth">
+          <div className="auth_container">
+            <img src={logo} alt="logo" />
+            <h1 className="auth_title"> Helo </h1>
+            <div className="auth_input_box">
+              <p> Username:</p>
+              <input value="" />
+            </div>
+            <div className="auth_input_box">
+              <p>Password:</p>
+              <input type="password" value="" />
+            </div>
+            <div className="auth_button_container">
+              <button className="button_form">Login</button>
+              <button className="button_form">Register</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
